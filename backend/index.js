304892834cfaa544f5b3098e2428edf8af8c442f
Yogi1305 <yogesh.22273@knit.ac.in>
2025-01-17 13:28:31 +0530

    adding frontend url

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app=express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-app.use(cors({ origin: 'http://localhost:3000' ,
+app.use(cors({ origin: 'https://linkly13.vercel.app' ,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     
     credentials:true
